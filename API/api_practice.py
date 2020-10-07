c40d5b814c2e3ee24915c0e545f1f7b53528d4be
Chelsea R <chelsea.riccobelli@acd.ccac.edu>
2020-10-06 23:27:37 -0400

    added API practice file

---
+++ API/api_practice.py
@@ -0,0 +1,17 @@
+import requests, json
+
+my_key = 'f7d58ebc'
+
+def movie_search(query, year):
+    api_key = my_key
+    url = 'http://www.omdbapi.com/'
+    title = query.replace(' ', '+')
+    final_url = url + '?t=' + title + '&y=' + year + '&apikey=' + api_key
+    req = requests.get(final_url)
+
+    apiDict = json.loads(req.text)
+    
+    for key, value in apiDict.items():
+        print(key + ":", value)
+
+movie_search('Titanic', '1997')
